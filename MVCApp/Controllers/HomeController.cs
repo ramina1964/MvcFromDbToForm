ded8e102936cc9129b1695c234ee55718528dfc1
Ramin Anvar <ramina1964@gmail.com>
2022-08-24 14:58:48 +0200

    Add project files.

---
+++ MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models;
+using System.Diagnostics;
+
+namespace MVCApp.Controllers;
+
+public class HomeController : Controller
+{
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult SignUp()
+    {
+        ViewBag.Message = "Employee Sign Up";
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult SignUp(EmployeeModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+
+    private readonly ILogger<HomeController> _logger;
+}
---
+++ MVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCApp.Models;
+
+public class EmployeeModel
+{
+    [Display(Name = "Employee Id")]
+    [Required(ErrorMessage = "Employee ID is required.")]
+    [Range(minimum: 100000, maximum: 999999, ErrorMessage = "Invalid Employee Id.")]
+    public int Id { get; set; }
+
+    [Display(Name = "First Name")]
+    [Required(ErrorMessage = "First name is required.")]
+    public string? FirstName { get; set; }
+
+    [Display(Name = "Last Name")]
+    [Required(ErrorMessage = "Last name is required.")]
+    public string? LastName { get; set; }
+
+    [DataType(DataType.EmailAddress)]
+    [Display(Name = "Email Address")]
+    [Required(ErrorMessage = "Email Address is required,")]
+    public string? EmailAddress { get; set; }
+
+    [Display(Name = "Confirm Email")]
+    [Compare("EmailAddress", ErrorMessage = "The Email Address and confirm email must match.")]
+    public string? ConfirmEmail { get; set; }
+
+    [Display(Name = "Password")]
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 10, ErrorMessage = "Password length must be at least 10 character long.")]
+    public string? Password { get; set; }
+
+    [Display(Name = "Confirm Password")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Password and confirm password must match.")]
+    public string? ConfirmPassword { get; set; }
+}
