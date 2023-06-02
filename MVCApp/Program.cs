76c79a3099bc5006f26ddf5b07f8fd800cfa1d45
Ramin Anvar <ramina1964@gmail.com>
2023-06-02 11:13:12 +0200

    Small changes

--- MVCApp/Program.cs
+++ MVCApp/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddTransient<IEmployeeService, EmployeeService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() == false)
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
