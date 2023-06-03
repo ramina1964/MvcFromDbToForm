0c0acb02fd9aa94be38c398eafbe4f13ee32ecd5
Ramin Anvar <ramina1964@gmail.com>
2023-06-04 01:31:13 +0200

    Uninstalled these NuGet packages:   - Microsoft.EntityFrameworkCore.SqlServer   - Microsoft.EntityFrameworkCore.InMemory

---
+++ MvcApp.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MvcApp.IntegrationTests;
+
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+}
+
